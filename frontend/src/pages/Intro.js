0951233a4e466e8025bba542bde079e93aeb3ec2
Shi He Wang <kiiwork@outlook.com>
2023-10-14 16:06:35 -0400

    Intro.js layout

--- frontend/src/pages/Intro.js
+++ frontend/src/pages/Intro.js
@@ -7,45 +7,76 @@ const Intro = () => {
       <div className="main">
         <Banner />
         <div className="content">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, eius animi! Esse, voluptas facilis
-          dolores ipsum ratione alias voluptatem sunt hic deserunt possimus qui rerum exercitationem accusantium quis
-          porro illo illum magnam blanditiis minima quasi repellat. Quos velit illum blanditiis placeat voluptas fuga
-          atque veniam accusamus cumque id debitis distinctio laboriosam illo aliquid neque quis exercitationem est nam
-          harum dolor inventore, odio maiores eaque temporibus. Eos, itaque. Veritatis maxime, a ipsam at pariatur
-          accusantium enim! Iusto nulla temporibus, consequuntur, accusantium natus nisi eligendi perspiciatis laborum
-          possimus perferendis error tempora dicta blanditiis, voluptas atque vero? Repudiandae fugiat odit doloremque,
-          quis excepturi deleniti explicabo reprehenderit ipsa ad praesentium, nobis porro. Soluta temporibus harum non,
-          illum ipsa, ratione eligendi porro asperiores laborum, neque culpa iure optio magni tempore beatae quibusdam
-          aut laudantium. Optio, molestiae perferendis repellendus ducimus ad excepturi earum, quae iure mollitia veniam
-          tempore velit magni. Corrupti doloribus obcaecati natus consequatur facere, perferendis qui officiis! Eum
-          eveniet perspiciatis et dolore repellendus eaque earum sunt iste repudiandae similique in, nemo, dolorum
-          recusandae animi consequatur a, necessitatibus tempora maiores error unde culpa nostrum. Sed eos quisquam esse
-          nostrum quidem in doloremque rem, eligendi expedita provident consectetur beatae repellat, reprehenderit
-          doloribus nobis iure, delectus repudiandae illo totam dolorum nemo deserunt facilis tenetur. Nobis, labore
-          distinctio quod vitae cumque velit quae laudantium veritatis, repellendus harum esse perspiciatis dolore
-          reprehenderit cum corporis earum nesciunt soluta? Necessitatibus ipsa deserunt tempora reiciendis pariatur
-          nihil quidem provident veniam adipisci, quas eos magnam omnis non unde amet tenetur ut beatae voluptatibus
-          possimus nostrum dignissimos deleniti corporis error quod? Odio nobis dolorum velit aliquam? Excepturi ut
-          fugiat doloribus ducimus sunt? Eligendi consequatur voluptas animi amet unde rerum repudiandae similique
-          blanditiis voluptate earum, natus, rem praesentium provident nulla, ducimus ut. Aspernatur dolorem
-          exercitationem aliquam quos nam tempore earum consequuntur eaque illum, ex provident velit placeat harum
-          voluptas magnam vitae. Non alias odit consectetur modi excepturi, fuga, consequatur quibusdam quasi nulla,
-          dolore possimus? Laboriosam, nemo recusandae, assumenda numquam, repudiandae nostrum ipsum omnis obcaecati
-          tempore at molestias voluptatum? Modi totam beatae distinctio, praesentium blanditiis neque enim fugiat,
-          aperiam, atque iusto repudiandae quisquam magni corporis. Pariatur corporis labore ab similique ipsum amet
-          tempora incidunt natus voluptas quis, animi voluptatum illum blanditiis molestias accusamus quod ipsa quisquam
-          possimus, illo ratione autem! Vel ea voluptatem asperiores ipsum, nemo deleniti error quasi, esse id,
-          assumenda nobis animi? Laboriosam sit hic fugiat consequatur inventore perspiciatis corrupti vitae nam
-          suscipit amet!
+          <section className="restaurant-info">
+            <h1>The Rich Uncle Tavern</h1>
+            <div className="restaurant-info-wrapping">
+              <div className="icon-p-wrapper">
+                <div className="icon-wrapper">
+                  <span className="material-icons-outlined material-symbols-outlined">restaurant</span>
+                </div>
+                <p>Gastro Pub, Canadian, Burgers</p>
+              </div>
+              <div className="icon-p-wrapper">
+                <div className="icon-wrapper">
+                  <span className="material-icons-outlined material-symbols-outlined">payments</span>
+                </div>
+                <p>Credit Card, Mastercard, UnionPay via TheFork Pay, Visa</p>
+              </div>
+              <div className="icon-p-wrapper">
+                <div className="icon-wrapper">
+                  <span className="material-icons-outlined material-symbols-outlined">storefront</span>
+                </div>
+                <p>
+                  45 King St W, Kitchener, ON N2G 1A1 <a href="#">View Map</a>
+                </p>
+              </div>
+              <div className="icon-p-wrapper">
+                <div className="icon-wrapper">
+                  <span className="material-icons-outlined material-symbols-outlined">call</span>
+                </div>
+                <p>
+                  <a href="#">(519) 208-8555</a>
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <section className="restaurant-content">
+            <div className="restaurant-content-wrapping">
+              <p>
+                With a unique, inventive menu, The Rich Uncle Tavern is built on hearty live-fire fare and shareable
+                snacks that pay homage to the brasseries and taverns of yesteryear. Defined by our humble and wholesome
+                approach to food and beverage, The Rich Uncle Tavern features a charming and homely ambience for patrons
+                to gather with old friends and find some new ones.
+              </p>
+              <p>
+                Whether you’re partaking in a crafted cocktail during live music or a late-night bite in one of our
+                booths, you’ll be able to experience a sociable atmosphere, curated beverages and delicious fare that
+                will leave you sated.
+              </p>
+            </div>
+          </section>
+
+          <section className="restaurant-menu">
+            <div className="restaurant-menu-wrapping">
+              <h2>Menu</h2>
+            </div>
+          </section>
         </div>
         <div className="sidebar">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quaerat debitis possimus, corrupti facilis sunt,
-          voluptate tenetur deserunt est quas laudantium in! Nulla obcaecati error cupiditate odio consequuntur nostrum?
-          Sint incidunt error veritatis accusantium eum a dicta pariatur maiores hic ad rerum aspernatur temporibus
-          laboriosam, unde suscipit quasi nisi! Pariatur eligendi, veritatis nemo sapiente quasi placeat accusamus iste
-          nostrum facere non aut praesentium animi voluptatum, necessitatibus accusantium eum voluptatibus. Blanditiis
-          maiores, repellat odio accusantium voluptatibus non maxime explicabo minus sit magni rem, atque quaerat rerum
-          deserunt.
+          <section className="reservation-info">
+            <h2>Make A Reservation</h2>
+            <div className="reservation-info-wrapping">
+              <form action="" method="" className="reservation-form"></form>
+            </div>
+          </section>
+
+          <section className="restaurant-hours">
+            <h2>Make A Reservation</h2>
+            <div className="reservation-hours-wrapping">
+              <table className="hours-table"></table>
+            </div>
+          </section>
         </div>
       </div>
     </main>
