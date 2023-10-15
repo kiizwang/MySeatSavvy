1c239749a9589d728425e74828476745dde6e329
Shi He Wang <kiiwork@outlook.com>
2023-10-14 22:53:30 -0400

    Intro.js - right side layout

--- frontend/src/pages/Intro.js
+++ frontend/src/pages/Intro.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import Banner from "../components/Banner.js";
 
 const Intro = () => {
+
+  const [restaurant, setRestaurant] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/api/restaurants")
+      .then((response) => {
+        setRestaurant(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  if (!restaurant) return <div>Loading...</div>;
+
   return (
     <main>
       <div className="main">
         <Banner />
         <div className="content">
           <section className="restaurant-info">
+            {/* <h1>{restaurant.name}</h1> */}
             <h1>The Rich Uncle Tavern</h1>
             <div className="restaurant-info-wrapping">
               <div className="icon-p-wrapper">
@@ -67,14 +84,95 @@ const Intro = () => {
           <section className="reservation-info">
             <h2>Make A Reservation</h2>
             <div className="reservation-info-wrapping">
-              <form action="" method="" className="reservation-form"></form>
+              <form action="" method="" className="reservation-form">
+                <div>
+                  <label>Party Size</label>
+                  <select id="party_size" defaultValue="2 people">
+                    <option>1 person</option>
+                    <option>2 people</option>
+                    <option>3 people</option>
+                    <option>4 people</option>
+                    <option>5 people</option>
+                    <option>6 people</option>
+                    <option>7 people</option>
+                    <option>8 people</option>
+                  </select>
+                </div>
+                <div>
+                  <label>Date</label>
+                  <input type="date" id="reservation_date" defaultValue={new Date().toISOString().split("T")[0]} />
+                </div>
+                <div>
+                  <label>Time</label>
+                  <select id="reservation_time" defaultValue="7:00 p.m.">
+                    <option>5:00 p.m.</option>
+                    <option>5:30 p.m.</option>
+                    <option>6:00 p.m.</option>
+                    <option>6:30 p.m.</option>
+                    <option>7:00 p.m.</option>
+                    <option>7:30 p.m.</option>
+                    <option>8:00 p.m.</option>
+                    <option>8:30 p.m.</option>
+                    <option>9:00 p.m.</option>
+                    <option>9:30 p.m.</option>
+                    <option>10:00 p.m.</option>
+                    <option>10:30 p.m.</option>
+                    <option>11:00 p.m.</option>
+                    <option>11:30 p.m.</option>
+                  </select>
+                </div>
+                <div>
+                  <input type="submit" value="Submit" />
+                </div>
+              </form>
             </div>
           </section>
 
           <section className="restaurant-hours">
-            <h2>Make A Reservation</h2>
+            <h2>Hours of Operation</h2>
             <div className="reservation-hours-wrapping">
-              <table className="hours-table"></table>
+              <table className="hours-table">
+                <tr>
+                  <td>Monday</td>
+                  <td>11:30 a.m. - 03:00 p.m.</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>05:30 p.m. - 22:30 p.m.</td>
+                </tr>
+                <tr>
+                  <td>Tuesday</td>
+                  <td>Closed</td>
+                </tr>
+                <tr>
+                  <td>Wednesday</td>
+                  <td>Closed</td>
+                </tr>
+                <tr>
+                  <td>Thursday</td>
+                  <td>11:30 a.m. - 03:00 p.m.</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>05:30 p.m. - 22:30 p.m.</td>
+                </tr>
+                <tr>
+                  <td>Friday</td>
+                  <td>11:30 a.m. - 03:00 p.m.</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>05:30 p.m. - 22:30 p.m.</td>
+                </tr>
+                <tr>
+                  <td>Saturday</td>
+                  <td>11:30 a.m. - 03:00 p.m.</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>05:30 p.m. - 22:30 p.m.</td>
+                </tr>
+              </table>
             </div>
           </section>
         </div>
